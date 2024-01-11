980fcc7ddc57518079a06f1343c50dde6c859908
sofie114 <84742096+sofie114@users.noreply.github.com>
2024-01-11 06:09:14 +0100

    Create rhyme_power.cpp

---
+++ open/rhyme_power.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define umap unordered_map
+#define uset unordered_set
+#define vo vector
+#define pb push_back
+#define ar array
+#define sz(x) x.size()
+#define rs(x, a) x.resize(a); 
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+
+#define rep(i, a, b) for(ll i=(a); i<b; i++)
+#define pr1(x) cerr << #x << '=' << x << ' ';
+#define pren cerr << endl;
+//what i dont use for no-google contests
+#define all(v) v.begin(), v.end()
+#define repd(i, a, b) for(ll i=(b-1); i >= a; i--)
+#define trav(item, arr) for (auto &item: arr)
+void _pr(int x) {cerr << x;}
+void _pr(long long x) {cerr << x;}
+void _pr(unsigned long long x) {cerr << x;}
+void _pr(double x) {cerr << x;}
+void _pr(char x) {cerr << '\'' << x << '\'';}
+void _pr(const char* x) {cerr << x;}
+void _pr(bool x) {cerr << (x ? "true" : "false");}
+template<typename T, typename V> void _pr(const pair<T, V> &x);
+template<typename T, typename V> void _pr(const pair<T, V> &x) {cerr << "\e[95m" << "[ "; _pr(x.first); cerr << ", "; _pr(x.second); cerr << "\e[95m" << ']';}
+template<typename T> void _pr(const T &x) {int f=0; cerr << '{'; for(auto &i: x) cerr << (f++ ? ", " : ""), _pr(i); cerr << "\e[91m" << '}';}
+template <typename T, typename... V> void _pr(T t, V... v) {_pr(t); if(sizeof...(v)) cerr << ", "; _pr(v...);}
+#define pr(x...) cerr << "\e[91m" << __func__ << ':' << __LINE__ << " [" << #x << "] = ["; _pr(x); cerr << "\e[91m" << ']' << "\033[0m"  << endl;
+
+//go for outline with ;, then details
+int const mxn = 1e6;
+int n, trie[mxn], ans;
+
+struct Trie {
+    map<char, Trie> children;
+    bool endOfWord = false; 
+    int ct = 0, kids=0;
+
+
+    void add(const string& s, int pos) {
+        if (pos != sz(s)) {
+            children[s[pos]].add(s, pos + 1);
+            if(children[s[pos]].ct == 1) kids += children[s[pos]].kids + children[s[pos]].endOfWord;
+        }
+        else endOfWord = true;
+        ct++;
+
+        if(kids>1 && ct >=2) {
+            ans = max(ans, pos);
+        }
+
+    }
+
+};
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+    cin>>n;
+    Trie trie;
+    rep(i, 0, n){
+        string s; cin>>s;
+        reverse(s.begin(), s.end());
+        trie.add(s, 0);
+    }
+
+    cout<<ans;
+}
+
+/*
+
+struct Trie {
+    map<char, Trie> children;
+    bool endOfWord = false; 
+
+    void add(const string& s, int pos) {
+        if (pos != sz(s)) children[s[pos]].add(s, pos + 1);
+        else endOfWord = true;
+    }
+
+};
+*/
